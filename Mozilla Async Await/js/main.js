709c67b40314cf57c42d40405879069b39c87fdc
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-08 13:44:16 +0100

    combingin Multiple Promises with Promise.all( )

--- Mozilla Async Await/js/main.js
+++ Mozilla Async Await/js/main.js
@@ -135,9 +135,31 @@ getPromise3
   .then((data) => {
     console.log(data[0].name);
   })
-  .catch((error) => console.error(`could not get error: ${error}`));
+  .catch((error) => console.error(`could not get response: ${error}`));
 
 //To support error handling, Promise objects provide a catch() method.
 //This is a lot like then(): you call it and pass in a handler function.
 //However, while the handler passed to then() is called when the asynchronous
 //operation succeeds, the handler passed to catch () is called when the asynchronous operation fails.
+
+
+//COMBINING MULTIPLE PROMISES
+const fetchPromise1 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise2 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise3 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((reponses) => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.log(`could not get response: ${error}`);
+  });
