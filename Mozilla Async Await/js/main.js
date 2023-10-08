d0e764170976f795239497e990430517672af0b5
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-08 13:54:02 +0100

    Using Promise.any( )

--- Mozilla Async Await/js/main.js
+++ Mozilla Async Await/js/main.js
@@ -142,7 +142,6 @@ getPromise3
 //However, while the handler passed to then() is called when the asynchronous
 //operation succeeds, the handler passed to catch () is called when the asynchronous operation fails.
 
-
 //COMBINING MULTIPLE PROMISES
 const fetchPromise1 = fetch(
   "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
@@ -155,11 +154,38 @@ const fetchPromise3 = fetch(
 );
 
 Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
-  .then((reponses) => {
+  .then((responses) => {
     for (const response of responses) {
       console.log(`${response.url}: ${response.status}`);
     }
   })
   .catch((error) => {
     console.log(`could not get response: ${error}`);
+  });
+//in this way, its all or nthing. That is, the promise.all throws an error if one of the promises is rejected
+//this does not include a 404 status response
+
+
+//PROMISE.ANY()
+// Sometimes, you might need any one of a set of promises to be fulfilled, and don
+// 't care which one. In that case, you want Promise.any(). This is like Promise.all(),
+// except that it is fulfilled as soon as any of the array of promises is fulfilled,
+//   or rejected if all of them are rejected:
+
+const fetchPromise4 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise5 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise6 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.any([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((response) => {
+    console.log(`${response.url}: ${response.status}`);
+  })
+  .catch((error) => {
+    console.error(`Failed to fetch: ${error}`);
   });
