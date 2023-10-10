e910cce6ae674512634e2f7f649f441470d1e283
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-10 22:57:58 +0100

    read file, renamed file

--- Traversy Node crash course/references/fs_demo.js
+++ Traversy Node crash course/references/fs_demo.js
@@ -2,26 +2,47 @@ const fs = require("fs");
 const path = require("path");
 
 //create folder
-fs.mkdir(path.join(__dirname, "/test"), {}, function (err) {
-  if (err) throw err;
-  console.log("folder created");
-});
+// fs.mkdir(path.join(__dirname, "/test"), {}, function (err) {
+//   if (err) throw err;
+//   console.log("folder created");
+// });
 
 //create and write file
-fs.writeFile(
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("file written into...");
+//     //create and write file
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "hello.txt"),
+//       "Hello World",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("file written into...");
+//       }
+//     );
+//   }
+// );
+
+//read file
+// fs.readFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "utf8",
+//   function (err, data) {
+//     if (err) throw err;
+//     console.log(data);
+//   }
+// );
+
+//renamefile
+
+fs.rename(
   path.join(__dirname, "/test", "hello.txt"),
-  "Hello World",
-  (err) => {
+  path.join(__dirname, "/test", "hellotoyou.txt"),
+  function (err) {
     if (err) throw err;
-    console.log("file written into...");
-    //create and write file
-    fs.appendFile(
-      path.join(__dirname, "/test", "hello.txt"),
-      "Hello World",
-      (err) => {
-        if (err) throw err;
-        console.log("file written into...");
-      }
-    );
+    console.log("rename file...");
   }
 );
