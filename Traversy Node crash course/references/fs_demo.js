cb5e687df21e51c128c3f98f1511bd6a5c6d2b44
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-10 22:47:29 +0100

    create and write file

--- Traversy Node crash course/references/fs_demo.js
+++ Traversy Node crash course/references/fs_demo.js
@@ -6,3 +6,13 @@ fs.mkdir(path.join(__dirname, "/test"), {}, function (err) {
   if (err) throw err;
   console.log("folder created");
 });
+
+//create and write file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World",
+  (err) => {
+    if (err) throw err;
+    console.log("file written into...");
+  }
+);
