acbb3d6d0b64901626247ea7bbc0c0d089fcf2f9
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-11 16:23:38 +0100

    added url_demo

--- Traversy Node crash course/references/url_demo.js
+++ Traversy Node crash course/references/url_demo.js
@@ -1,3 +1,14 @@
-const url = require('url')
+const url = require("url");
 
-const myUrl = new URL()
+const myUrl = new URL(
+  "https://100devsfollowalong.netlify.app/classes/class-33.html?id=2345&status=active"
+);
+
+// console.log(myUrl.href);
+// console.log(myUrl.toString());
+console.log(myUrl.host); //rootdomain
+console.log(myUrl.hostname);
+console.log(myUrl.pathname);
+console.log(myUrl.search);
+console.log(myUrl.searchParams);
+myUrl.searchParams.forEach((key,value)=> console.log(`${key} : ${value}`));
