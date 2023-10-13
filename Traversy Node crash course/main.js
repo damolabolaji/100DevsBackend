599aae1ee61401f13f6fc4daef2f378ab2ecf8dd
damolabolaji <bolaji.oyindamola@gmail.com>
2023-10-13 23:44:29 +0100

    created PORT in main.js

--- Traversy Node crash course/main.js
+++ Traversy Node crash course/main.js
@@ -1,18 +1,24 @@
-const Logger = require("./logger");
+// const mainLogger = new Logger();
+// mainLogger.on("message", (data) => {
+//   fs.appendFile(
+//     path.join(__dirname, "./references/mytest", "uuids.txt"),
+//     `${data.id}:${data.msg}\n`,
+//     (err) => {
+//       if (err) throw err;
+//       console.log("file written into...");
+//     }
+//   );
+// });
+
+// mainLogger.log("suck it");
+// mainLogger.log("suck");
+
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 
-const mainLogger = new Logger();
-mainLogger.on("message", (data) => {
-  fs.appendFile(
-    path.join(__dirname, "./references/mytest", "uuids.txt"),
-    `${data.id}:${data.msg}\n`,
-    (err) => {
-      if (err) throw err;
-      console.log("file written into...");
-    }
-  );
+const server = http.createServer((req, res) => {
+  console.log(req.url);
 });
-
-mainLogger.log("suck it");
-mainLogger.log("suck");
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`server running on ${PORT}`));
